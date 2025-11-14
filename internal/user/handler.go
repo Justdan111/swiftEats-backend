3c20e16fd84cdf1a68755d4316bbd5a160847bc8
justdan111 <emmanuelngulube292@gmail.com>
2025-11-14 20:49:00 +0100

    new

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,1 +1,44 @@
 package user
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(s *Service) *Handler {
+	return &Handler{s}
+}
+
+func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	json.NewDecoder(r.Body).Decode(&req)
+	err := h.service.Register(req.Email, req.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "user created"})
+}
+
+
+func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	json.NewDecoder(r.Body).Decode(&req)
+	token, err := h.service.Login(req.Email, req.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
