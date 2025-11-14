1f57e200f009f1fec6b4930107d67ea28a4dd7dd
justdan111 <emmanuelngulube292@gmail.com>
2025-11-14 15:52:32 +0100

    Refactor user model and repository to include CreatedAt field and enhance user management functionality

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,1 +1,25 @@
 package user
+
+import (
+	"database/sql"
+)
+
+type Repository struct {
+	DB *sql.DB
+}
+
+func NewRepository(db *sql.DB) *Repository {
+	return &Repository{DB: db}
+}
+
+func (r *Repository) CreateUser(email, hashedPassword string) error {
+		_, err := r.DB.Exec(`INSERT INTO users (email, password) VALUES ($1, $2)`, email, hashedPassword)
+		return err
+} 
+
+func (r *Repository) GetUserByEmail(email string) (*User, error) {
+	row := r.DB.QueryRow(`SELECT id, email, password, created_at FROM users WHERE email=$1`, email)
+	u := &User{}
+	err := row.Scan(&u.ID, &u.Email, &u.Password, &u.CreatedAt)
+	return u, err
+}
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,46 @@
 package user
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Service struct {
+	repo 	*Repository
+	jwtSecret []byte
+}
+
+func NewService(repo *Repository, jwtSecret []byte) *Service {
+	return  &Service{repo, jwtSecret}
+}
+
+func (s *Service) RegisterUser(email, password string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	
+	return s.repo.CreateUser(email, string(hashed))
+
+}
+
+func (s *Service) Login(email, password string) (string, error) {
+	u, err := s.repo.GetUserByEmail(email)
+	if err != nil {
+		return "", errors.New("invalid credentials")
+	}
+	if bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)) != nil {
+		return "", errors.New("invalid credentials")
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": u.ID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	})
+
+	return token.SignedString(s.jwtSecret)
+}
+
