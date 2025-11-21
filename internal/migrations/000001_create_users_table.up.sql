40c2591ccd48b17c925ae6fa691caea0c67493c5
justdan111 <emmanuelngulube292@gmail.com>
2025-11-21 23:14:00 +0100

    add sql table

--- internal/migrations/000001_create_users_table.up.sql
+++ internal/migrations/000001_create_users_table.up.sql
@@ -1,8 +1,10 @@
-CREATE TABLE users (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  email TEXT UNIQUE NOT NULL,
-  password_hash TEXT NOT NULL,
-  name TEXT,
-  phone TEXT,
-  created_at TIMESTAMPTZ DEFAULT now()
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    name TEXT,
+    phone TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
