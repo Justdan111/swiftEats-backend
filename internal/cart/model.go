9df54b86e69c17427dfcaaae8337d85ec5da5b6d
justdan111 <emmanuelngulube292@gmail.com>
2026-01-27 19:33:59 +0100

    Add Cart and CartItem models with JSON annotations

--- internal/cart/model.go
+++ internal/cart/model.go
@@ -1,1 +1,20 @@
 package cart
+
+import "time"
+
+type Cart struct {
+	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
+	RestaurantID string    `json:"restaurant_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type CartItem struct {
+	ID 	  string    `json:"id"`
+	CartID   string    `json:"cart_id"`
+	ManueItemID string    `json:"menu_item_id"`
+	Quantity int 	 `json:"quantity"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
